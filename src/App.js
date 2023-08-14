5eaeea24c9ce4bfae7dd8f268ac83619c8107713
Janneth Gutierrez <jgutier1@gmail.com>
2023-08-14 00:08:51 -0400

    second commit: App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+const gownsList = [
+  {
+    id: 0,
+    name: "Enchanted White Dress",
+    rating: null,
+    category: "Wedding Gown",
+    color: "white",
+    material: "lace"
+  },
+  {
+    id: 1,
+    name: "Lavender Haze",
+    rating: null,
+    category: "Ball Gown",
+    color: "purple",
+    material: "satin"
+  },
+  {
+    id: 2,
+    name: "Midnight Rain",
+    rating: null,
+    category: "Evening Gown",
+    color: "black",
+    material: "silk"
+  },
+  {
+    id: 3,
+    name: "Bejeweled",
+    rating: null,
+    category: "Prom Gown",
+    color: "red",
+    material: "velvet"
+  },
+  {
+    id: 4,
+    name: "Wildest Dreams",
+    rating: null,
+    category: "Wedding Gown",
+    color: "white",
+    material: "silk"
+  },
+  {
+    id: 5,
+    name: "Cruel Summer",
+    rating: null,
+    category: "Outdoor Gown",
+    color: "yellow",
+    material: "floral print"
+  },
+  {
+    id: 6,
+    name: "Bejeweled",
+    rating: null,
+    category: "Prom Gown",
+    color: "red",
+    material: "blue"
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>My Gowns of Love Project</h1>
+     { gownsList.map ( gown => (
+      <div>
+      <h4>{gown.name}</h4>
+      <p>{gown.color}</p>
+      <p>{gown.category}</p>
+      
+      </div>
+      ) ) }
     </div>
   );
 }
