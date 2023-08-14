5ff3972a5d3cb028998e522b23d6333df11877f2
Janneth Gutierrez <jgutier1@gmail.com>
2023-08-14 00:31:35 -0400

    my project

--- src/GownCard.js
+++ src/GownCard.js
@@ -1,6 +1,6 @@
 function GownCard(props) {
     return (
-        <div>
+        <div className="bg-light border p-4 m-2">
         <h4>{props.gown.name}</h4>
         <p>{props.gown.color}</p>
         <p>{props.gown.category}</p>      
